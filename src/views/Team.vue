fe41977afaa0c1c725652ba272c6523a687e81f3
Jiwo <mdf.jiwo@gmail.com>
2019-04-21 18:34:39 +0700

    first

---
+++ src/views/Team.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Team">
+    <h1 class="subheading grey--text">Team</h1>
+
+  <v-container fluid class="my-5">
+    content
+  </v-container>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
