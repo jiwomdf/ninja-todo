d908e2b301696fe2e43489437c142258ad51ad5d
jiwomdf <mdf.jiwo@gmail.com>
2019-04-22 15:23:03 +0700

    #26

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <nav>
         <v-toolbar flat app>
+             <!-- side menu -->
             <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer">
                 
             </v-toolbar-side-icon>
@@ -11,14 +12,44 @@
             </v-toolbar-title>
 
             <VSpacer/>
-        
+
+            <!-- dropdown menu -->
+            <v-menu offset-y>
+                <v-btn flat color="grey" slot="activator">
+                    <v-icon>expand_more</v-icon>
+                    <span>Menu</span>
+                </v-btn>
+                <v-list>
+                    <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+
+             <!-- button logout -->
             <v-btn flat color="grey">
                 <span>Sign Out</span>
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
+
         </v-toolbar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
+             <!-- Profile picture -->
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <img src="/avatar-1.png" alt="">
+                    </v-avatar>
+                    <p class="white--text subheading mt-1">
+                        The Net Ninja
+                    </p>
+                </v-flex>
+                <v-flex class="mt-4 mb-3">
+                    <Popup />
+                </v-flex>
+            </v-layout>
+             <!-- User menu -->
             <v-list>
                 <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
                     <v-list-tile-action>
@@ -37,7 +68,13 @@
 
 
 <script>
+// eslint-disable-next-line
+/* eslint-disable */
+
+import Popup from './Popup'
+
 export default {
+    components: { Popup },
     data(){
         return{
             drawer:false,
