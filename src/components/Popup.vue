353bdde9b1eb842aab4c1f378cf4984b5db34f69
jiwomdf <mdf.jiwo@gmail.com>
2019-04-24 08:51:23 +0700

    add validation form in Popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,12 +6,12 @@
                 <h2>Add a New Project</h2>
             </v-card-title>
             <v-card-text>
-                <v-form class="px-3">
-                    <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-                    <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+                    <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
 
                     <v-menu>
-                        <v-text-field :value="formattedDate" slot="activator" label="Due date" prepend-icon="date_range"></v-text-field>
+                        <v-text-field :value="formattedDate" slot="activator" label="Due date" prepend-icon="date_range" :rules="inputRules"></v-text-field>
                         <v-date-picker v-model="due"></v-date-picker>
                     </v-menu>
 
@@ -33,14 +33,20 @@ import  format  from "date-fns/format";
 export default {
     data(){
         return {
-            title : "",
-            content: "",
-            due: null
+            title : '',
+            content: '',
+            due: null,
+            inputRules : [
+                v => v.length >= 3 || 'Minimum length is 3 characters'
+            ]
         }
     },
     methods : {
         submit(){
-            console.log(this.title, this.content)
+            if(this.$refs.form.validate()){
+                console.log(this.title, this.content)
+            }
+            
         }
     },
     computed : {
